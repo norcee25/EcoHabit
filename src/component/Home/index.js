29a3f3de4e52bdfeee159572b18d67f88fae84ca
avrilkamu <s22210220@student.unklab.ac.id>
2024-11-19 20:05:19 +0800

    edit firebase title home 100%

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -173,10 +173,10 @@ const Home = () => {
   <div className="container">
     <div className="row justify-content-center align-items-center">
       <div className="col-lg-5 col-12 ms-auto">
-        <h2 className="mb-0">Make an impact. <br /> Save lives.</h2>
+        <h2 className="mb-0">{home.er} <br /> {home.sl}</h2>
       </div>
       <div className="col-lg-5 col-12">
-        <a href="#section_4" className="custom-btn btn smoothscroll">Become a part of our Team</a>
+        <a href="#section_4" className="custom-btn btn smoothscroll">{home.bt}</a>
       </div>
     </div>
   </div>
